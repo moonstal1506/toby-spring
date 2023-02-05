b069840a4cc6e3d9f7bf0580dd57ef00202cfd93
moonstal1506 <moonstal1506@gmail.com>
2023-02-05 10:09:25 +0900

    스프링 컨테이너 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -21,24 +22,28 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
+        //스프링 컨테이너 생성
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class); //빈등록
+        //빈 만들기 구성정보 이용해서
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
 
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
+            servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     //인증, 보안, 다국어, 공통 기능
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        //서블릿 컨테이너는 헬로 컨트롤러 가 어떻게 만들어진지 모름
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
-                    } else if (req.getRequestURI().equals("/user")) {
-                        //
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
