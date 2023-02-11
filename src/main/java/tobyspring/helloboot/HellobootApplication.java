d75516b71f1f59597e2e6b58152a0b18b760e2c5
moonstal1506 <moonstal1506@gmail.com>
2023-02-11 15:14:27 +0900

    자바코드 구성 정보 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,11 +3,15 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -21,11 +25,22 @@ import java.io.IOException;
 매핑: 웹요청 정보 활용해서 어떤 로직을 수행하는 코드를 호출할 것인가
 바인딩: 헬로 컨트롤러 파라미터로 넘어온 name울 String타입으로 인자값을 넘겨줌 타입 변경
  */
+@Configuration
 public class HellobootApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService(){
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
         //스프링 컨테이너 생성
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -39,12 +54,9 @@ public class HellobootApplication {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class); //빈등록
-        applicationContext.registerBean(SimpleHelloService.class); //빈등록
-        //빈 만들기 구성정보 이용해서
+        //자바코드로된 구성정보 여기있다고 등록
+        applicationContext.register(HellobootApplication.class);
         applicationContext.refresh();
-
-
     }
 
 }
