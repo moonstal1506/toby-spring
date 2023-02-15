6f6df4fe6ed1d5196aab5f7bba2a0c718220bc7d
moonstal1506 <moonstal1506@gmail.com>
2023-02-15 23:20:00 +0900

    DI와 단위 테스트

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,8 @@
 package tobyspring.helloboot;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 
 //메타애노테이션: 애노테이션 위에 붙은 애노테이션
 //@MyComponent
@@ -14,17 +11,18 @@ import java.util.Objects;
 public class HelloController {
 
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return helloService.sayHello(Objects.requireNonNull(name));
+        if (name == null || name.trim().length() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return helloService.sayHello(name);
     }
 
 }
