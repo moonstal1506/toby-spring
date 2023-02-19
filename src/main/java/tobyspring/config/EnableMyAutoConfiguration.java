0f23726e62e78b071f1b85fc000e93484415e3a5
moonstal1506 <moonstal1506@gmail.com>
2023-02-19 11:51:07 +0900

    동적인 자동 구성 정보 등록

--- src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)//자바 애노테이션 기본은 class
 @Target(ElementType.TYPE)//클래스,인터페이스,enum
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})//컴포넌트가 붙은 애들 넣을 수 있음,
+@Import(MyAutoConfigImportSelector.class)//컴포넌트가 붙은 애들 넣을 수 있음,
 // import HellobootApplication 패키지 밖에 있는 애들 가져옴
 public @interface EnableMyAutoConfiguration {
 }
