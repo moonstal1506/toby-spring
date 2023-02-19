6e2f5fcf3949cf99d9b276525f8bf1332d412a55
moonstal1506 <moonstal1506@gmail.com>
2023-02-19 11:52:27 +0900

    충돌해결

--- src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -11,11 +11,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)//자바 애노테이션 기본은 class
 @Target(ElementType.TYPE)//클래스,인터페이스,enum
-<<<<<<< HEAD
 @Import(MyAutoConfigImportSelector.class)//컴포넌트가 붙은 애들 넣을 수 있음,
-=======
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})//컴포넌트가 붙은 애들 넣을 수 있음,
->>>>>>> origin/main
 // import HellobootApplication 패키지 밖에 있는 애들 가져옴
 public @interface EnableMyAutoConfiguration {
 }
