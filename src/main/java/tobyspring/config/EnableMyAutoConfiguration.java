6b6a45bbb4df6eed288dab54b1f729477332f262
moonstal1506 <moonstal1506@gmail.com>
2023-02-19 11:40:56 +0900

    인프라 빈 구성 정보의 분리

---
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -0,0 +1,17 @@
+package tobyspring.config;
+
+import org.springframework.context.annotation.Import;
+import tobyspring.config.autoconfig.DispatcherServletConfig;
+import tobyspring.config.autoconfig.TomcatWebServerConfig;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)//자바 애노테이션 기본은 class
+@Target(ElementType.TYPE)//클래스,인터페이스,enum
+@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})//컴포넌트가 붙은 애들 넣을 수 있음,
+// import HellobootApplication 패키지 밖에 있는 애들 가져옴
+public @interface EnableMyAutoConfiguration {
+}
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,14 +1,7 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MySpringBootApplication;
 
 
 /*
@@ -16,7 +9,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 매핑: 웹요청 정보 활용해서 어떤 로직을 수행하는 코드를 호출할 것인가
 바인딩: 헬로 컨트롤러 파라미터로 넘어온 name울 String타입으로 인자값을 넘겨줌 타입 변경
  */
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
