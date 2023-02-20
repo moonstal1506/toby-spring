4ac997459a01baffbe71f147541b45ad74d5e5da
moonstal1506 <moonstal1506@gmail.com>
2023-02-20 21:45:35 +0900

    자동 구성 애노테이션 적용

--- src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
@@ -3,8 +3,10 @@ package tobyspring.config.autoconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MyAutoConfiguration;
 
-@Configuration
+
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     // 스프링이 applicationContext 알아서 넣어줌
