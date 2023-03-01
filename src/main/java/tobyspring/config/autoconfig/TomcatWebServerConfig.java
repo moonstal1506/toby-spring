2a70708b574f67d4d335a0632ee5e13e8f5d916f
moonstal1506 <moonstal1506@gmail.com>
2023-03-01 13:10:27 +0900

    자동 구성에 Environmen 프로퍼티 적용

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
@@ -13,7 +14,12 @@ public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean //사용자가 등록한게 없으면 등록해라
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        //톰캣을 띄우는데 필요한 여러작업 지정
+        //Tomcat started on port(s): 8080 (http) with context path '/app'
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+//        factory.setContextPath("/app");
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 }
