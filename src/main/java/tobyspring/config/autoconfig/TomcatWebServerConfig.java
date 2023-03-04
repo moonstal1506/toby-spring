d2e1f79c4e6aee188511cb2a26b6646b52eab2d3
moonstal1506 <moonstal1506@gmail.com>
2023-03-04 16:17:50 +0900

    프로퍼티 빈의 후처리기 도입

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,16 +1,18 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Import;
 import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyAutoConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@Import(ServerProperties.class)
+@EnableMyAutoConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
 
 //    @Value("${contextPath:}")
